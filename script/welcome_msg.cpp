d964e853c128552dcee08586c54550f629e3e631
jeyhooon <jeyhoon.maskani@gmail.com>
2021-10-30 12:20:40 +0200

    updated code

--- script/welcome_msg.cpp
+++ script/welcome_msg.cpp
@@ -1,22 +1,23 @@
 
 #include <gazebo/gazebo.hh>
 
-
 namespace gazebo
 {
   class WorldPluginMyHome : public WorldPlugin
   {
-    public: WorldPluginMyHome() : WorldPlugin()
-            {
-              std::string your_name;
-              std::cout << "Please enter your name: ";
-              std::getline(std::cin, your_name);
-              std::cout << "Welcome to " << your_name << "'s world!" << std::endl;
-            }
+  public:
+    WorldPluginMyHome() : WorldPlugin()
+    {
+      std::string your_name;
+      std::cout << "Please enter your name: ";
+      std::getline(std::cin, your_name);
+      std::cout << "Welcome to " << your_name << "'s world!" << std::endl;
+    }
 
-    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-            {
-            }
+  public:
+    void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+    {
+    }
   };
   GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyHome)
 }
